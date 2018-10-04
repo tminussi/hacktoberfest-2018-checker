1a7faf3eee2894c7b961e22ffbc1aaf4ac4efe5d
Luc Chmielowski <luc.chmielowski@gmail.com>
2018-10-04 14:36:45 +0200

    Added new function to load translations from server

--- client/i18n.js
+++ client/i18n.js
@@ -1,33 +1,41 @@
 ($ =>
   $.when($.ready).then(() => {
-    i18next.init(
-      {
-        lng: "en",
-        resources: {
-          en: {
-            translation: {
-              alert_messag: "Type something !",
-              request_error: "Oops, something went wrong!",
-              label: {
-                username: "Username"
+    // Initialize i18next Object
+    i18next
+      .use(i18nextChainedBackend)
+      .use(i18nextBrowserLanguageDetector)
+      .init(
+        {
+          fallbackLng: "en",
+          ns: ["common"],
+          defaultNS: "common",
+          detection: {
+            order: ["querystring", "localStorage", "navigator"],
+            lookupQuerystring: "lng",
+            lookupLocalStorage: "i18nextLng",
+            caches: ["localStorage"]
+          },
+          backend: {
+            backends: [i18nextLocalStorageBackend, i18nextXHRBackend],
+            backendOptions: [
+              {
+                prefix: "i18next_res_",
+                expirationTime: 7 * 24 * 60 * 60 * 1000
               },
-              button: {
-                "submit-req": "Submit"
-              },
-              th: {
-                status: "Status",
-                link: "Link"
-              },
-              img: {
-                "profile-alt": "User photo"
+              {
+                loadPath: "http://localhost:3000/locale/{{lng}}",
+                crossDomain: true,
+                parse: data => {
+                  return JSON.parse(data);
+                }
               }
-            }
+            ]
           }
+        },
+        function(err, t) {
+          jqueryI18next.init(i18next, $);
+          // Get elements to translate
+          $(".container").localize();
         }
-      },
-      function(err, t) {
-        jqueryI18next.init(i18next, $);
-        $(".container").localize();
-      }
-    );
+      );
   }))(jQuery);
--- server/locales/index.js
+++ server/locales/index.js
@@ -1,9 +1,11 @@
 const i18n = require("i18next");
-const en = require("./resources/en.json");
+const en = require("./en/server.json");
 
 // Initialize i18next Object and inject translations
 i18n.init({
   lng: "en",
+  defaultNS: "common",
+  fallbackLng: "en",
   resources: {
     en
   }
