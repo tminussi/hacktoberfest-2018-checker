901d280fb4244891cbbc141048407579cfdf7053
Wessel Hendriks <hendriksw93@gmail.com>
2018-10-21 21:31:46 +0200

    Refactor magic strings in Github Service to constants
    Refactored magic strings in request and pull request validation to constants. The reason behind this refactoring is to make these values more obvious.

--- server/service/github-service.js
+++ server/service/github-service.js
@@ -1,88 +1,94 @@
 const request = require('request');
 
 const fetchPullRequestsOfUser = (username) => {
-    return new Promise((resolve, reject) => {
-        request.get({
-            url: `https://api.github.com/search/issues?q=author:${username}+type:pr`,
-            headers: {
-                Accept: 'application/vnd.github.cloak-preview',
-                'User-Agent': 'tminussi'
-            }
-        }, (err, data, body) => {
-            if (responseContainsClientOrServerError(data.statusCode)) {
-                const response = createErrorResponse(data.statusCode, body);
-                return reject(response);
-            } else {
-                const jsonBody = JSON.parse(body);
-                const validPullRequests = getValidPullRequestsFromResponseBody(jsonBody);
-                let response = null;
+  return new Promise((resolve, reject) => {
+    const API_PREVIEW_COMMIT_SEARCH = 'application/vnd.github.cloak-preview';
+    const GITHUB_USER_DOING_REQUEST = 'tminussi';
 
-                if (validPullRequests.length) {
-                    response = createValidPullRequestsResponse(validPullRequests);
-                } else if (!jsonBody.items.length) {
-                    response = createNoPullRequestsResponse();
-                } else {
-                    response = createNoValidPullRequestsResponse(jsonBody);
-                }
-                return resolve(response);
-            }
-        });
+    request.get({
+      url: `https://api.github.com/search/issues?q=author:${username}+type:pr`,
+      headers: {
+        Accept: API_PREVIEW_COMMIT_SEARCH,
+        'User-Agent': GITHUB_USER_DOING_REQUEST
+      }
+    }, (err, data, body) => {
+      if (responseContainsClientOrServerError(data.statusCode)) {
+        const response = createErrorResponse(data.statusCode, body);
+        return reject(response);
+      } else {
+        const jsonBody = JSON.parse(body);
+        const validPullRequests = getValidPullRequestsFromResponseBody(jsonBody);
+        let response = null;
+
+        if (validPullRequests.length) {
+          response = createValidPullRequestsResponse(validPullRequests);
+        } else if (!jsonBody.items.length) {
+          response = createNoPullRequestsResponse();
+        } else {
+          response = createNoValidPullRequestsResponse(jsonBody);
+        }
+        return resolve(response);
+      }
     });
+  });
 }
 
 const responseContainsClientOrServerError = (statusCode) => statusCode >= 400;
 
 const createErrorResponse = (statusCode, body) => {
-    return {
-        statusCode: statusCode,
-        body: JSON.parse(body)
-    };
+  return {
+    statusCode: statusCode,
+    body: JSON.parse(body)
+  };
 }
 
 const createNoPullRequestsResponse = () => {
-    return {
-        valid_pull_requests_amount: 0
-    };
+  return {
+    valid_pull_requests_amount: 0
+  };
 }
 
 const createValidPullRequestsResponse = (validPullRequests) => {
-    return {
-        valid_pull_requests_amount: validPullRequests.length,
-        avatar: validPullRequests[0].user.avatar_url,
-        pull_requests: validPullRequests.map(item => {
-            return {
-                url: item.html_url,
-                state: item.state
-            }
-        }),
-        message: createMessageByPullrequestQuantity(validPullRequests.length)
-    };
+  return {
+    valid_pull_requests_amount: validPullRequests.length,
+    avatar: validPullRequests[0].user.avatar_url,
+    pull_requests: validPullRequests.map(item => {
+      return {
+        url: item.html_url,
+        state: item.state
+      }
+    }),
+    message: createMessageByPullrequestQuantity(validPullRequests.length)
+  };
 }
 
 const getValidPullRequestsFromResponseBody = (responseBody) => {
-    return responseBody.items
-        .filter(item => new Date(item.created_at).getTime() > new Date('2018-10-01T00:00:00Z').getTime())
-        .filter(item => new Date(item.created_at).getTime() < new Date('2018-11-01T00:00:00Z').getTime());
+  const START_HACKTOBER_2018 = '2018-10-01T00:00:00Z';
+  const STOP_HACKTOBER_2018 = '2018-11-01T00:00:00Z';
+  
+  return responseBody.items
+    .filter(item => new Date(item.created_at).getTime() > new Date(START_HACKTOBER_2018).getTime())
+    .filter(item => new Date(item.created_at).getTime() < new Date(STOP_HACKTOBER_2018).getTime());
 }
 
 const createNoValidPullRequestsResponse = (jsonBody) => {
-    return {
-        valid_pull_requests_amount: 0,
-        avatar: jsonBody.items[0].user.avatar_url,
-        message: createMessageByPullrequestQuantity(0)
-    };
+  return {
+    valid_pull_requests_amount: 0,
+    avatar: jsonBody.items[0].user.avatar_url,
+    message: createMessageByPullrequestQuantity(0)
+  };
 }
 
 const createMessageByPullrequestQuantity = (quantity) => {
-    if(!quantity) {
-        return "You haven't created any Pull Requests so far. Start contributing to the Open Source Community and Win a T-Shirt at 5 PRs"
-    } else if (quantity >= 1 && quantity <= 3) {
-        return "Great job. The Open Source community is already thankful to you, but you can achieve more. Keep it up!"
-    } else if (quantity === 4) {
-        return "You are almost there! One more to go and you will be able to wear one of the coolest T-Shirts out there"
-    } else {
-        return "You did it! Congratulations! Time to rest and wait for your well deserved T-Shirt!"
-    }
+  if(!quantity) {
+    return "You haven't created any Pull Requests so far. Start contributing to the Open Source Community and Win a T-Shirt at 5 PRs"
+  } else if (quantity >= 1 && quantity <= 3) {
+    return "Great job. The Open Source community is already thankful to you, but you can achieve more. Keep it up!"
+  } else if (quantity === 4) {
+    return "You are almost there! One more to go and you will be able to wear one of the coolest T-Shirts out there"
+  } else {
+    return "You did it! Congratulations! Time to rest and wait for your well deserved T-Shirt!"
+  }
 }
 
 module.exports = {
